b39e1cb1a6509d9706e143a68da565f3051c7f43
siu <siukwan.kwok@georgebrown.ca>
2025-01-31 13:18:57 -0500

    update travis

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Build the project
 npm run build
+
+# Configure Git for the deployment
 git config user.name "a40856"
 git config user.email "siukwan.kwok@georgebrown.com"
+
+# Add the build directory to the Git staging area
 git add -f build
+
+# Commit the changes
 git commit -m "Deploy to GitHub Pages"
+
+# Push to the gh-pages branch using the GitHub token
 git push --force --quiet "https://${GITHUB_TOKEN}@github.com/a40856/comp2156.git" main:gh-pages
